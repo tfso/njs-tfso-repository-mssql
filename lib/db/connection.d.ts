547281a5bcb242447dc1d5a95f77b6b0d378b278
24SevenOffice <post@24sevenoffice.com>
2016-09-01 11:25:40 +0000

    Adding release files

---
+++ lib/db/connection.d.ts
@@ -0,0 +1,13 @@
+import * as MsSql from 'mssql';
+export default class Connection {
+    private _connectionString;
+    private _connection;
+    private _transaction;
+    private _rolledback;
+    constructor(connectionString: MsSql.config);
+    beginTransaction(): Promise<void>;
+    commitTransaction(): Promise<void>;
+    rollbackTransaction(): Promise<void>;
+    execute<U>(query: PromiseLike<U>): Promise<U>;
+    execute<U>(work: (connection: MsSql.Connection) => U | PromiseLike<U>): Promise<U>;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export { default as QueryRecordSet } from './db/queryrecordset';
+export { default as QueryStream } from './db/querystream';
+export { default as Connection } from './db/connection';
