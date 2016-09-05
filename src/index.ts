4872a90de00c94ec618bca81a2b24f399cb7755f
lostfields <ng@24sevenoffice.com>
2016-09-05 10:08:45 +0200

    changing token

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 ﻿export { default as QueryRecordSet } from './db/queryrecordset';
 export { default as QueryStream } from './db/querystream';
 
+export { Query } from 'tfso-repository/lib/repository/db/query';
+
 export { default as Connection } from './db/connection';
