3f1e4dea822e0299073669c53158349a27579dfb
lostfields <ng@24sevenoffice.com>
2016-09-07 13:19:04 +0200

    something strange in ES6 when extending classes from npm modules

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es2015-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/mssql/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
