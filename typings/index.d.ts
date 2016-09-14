d7d4b47b7ad369769cc6eae4db2349f6f0c4eb7d
lostfields <ng@24sevenoffice.com>
2016-09-14 13:11:42 +0200

    target ES6

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="globals/es2015-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/mssql/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
