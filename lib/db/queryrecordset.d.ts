79f93f0f6d3fd9b11fe2e34d963ef589782fc85f
24SevenOffice <post@24sevenoffice.com>
2016-10-05 06:33:30 +0000

    Adding release files

---
+++ lib/db/queryrecordset.d.ts
@@ -0,0 +1,15 @@
+import * as MsSql from 'mssql';
+import { Query } from 'tfso-repository/lib/repository/db/query';
+import { RecordSet } from 'tfso-repository/lib/repository/db/recordset';
+export declare abstract class QueryRecordSet<TEntity> extends Query<TEntity> {
+    private _connection;
+    private _transaction;
+    constructor(connection?: MsSql.Connection | MsSql.Transaction);
+    connection: MsSql.Transaction | MsSql.Connection;
+    protected input(name: string, value: any): void;
+    protected input(name: string, type: any, value: any): void;
+    protected executeQuery(): Promise<RecordSet<TEntity>>;
+    protected abstract transform(record: any): TEntity;
+}
+export default QueryRecordSet;
+export { RecordSet };
