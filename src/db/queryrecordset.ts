7898b891e519f37f888ba2cc609f7f91520f6bb3
lostfields <ng@24sevenoffice.com>
2017-05-09 09:28:58 +0200

    upgrading dependency

--- src/db/queryrecordset.ts
+++ src/db/queryrecordset.ts
@@ -96,11 +96,7 @@ export abstract class QueryRecordSet<TEntity> extends Query<TEntity> {
                         if (where) {
                             this.query.operations.remove(where);
 
-                            predicate = ((op: WhereOperator<TEntity>) => {
-                                return (entity: TEntity) => {
-                                    return op.predicate.apply({}, [entity].concat(op.parameters));
-                                }
-                            })(<WhereOperator<TEntity>>where);
+                            predicate = where.predicate;
                         }
 
                         entities = results.map(this.transform);
