9351cfc32dd4c795d8b8885de4c5a782c15909af
24SevenOffice <post@24sevenoffice.com>
2019-05-23 12:27:38 +0000

    Adding release files

---
+++ lib/db/connection.d.ts
@@ -0,0 +1,27 @@
+import * as MsSql from 'mssql';
+import { Query } from 'tfso-repository/lib/repository/db/query';
+import { IRecordSet } from 'tfso-repository/lib/repository/db/recordset';
+export declare enum IsolationLevel {
+    ReadUncommitted = 0,
+    ReadCommitted = 1,
+    RepeatableRead = 2,
+    Snapshot = 3,
+    Serializable = 4,
+}
+export default class Connection {
+    private static connectionPool;
+    private _connectionString;
+    private _connection;
+    private _transaction;
+    private _rolledback;
+    constructor(connectionString: MsSql.config | PromiseLike<MsSql.config>);
+    beginTransaction(isolationLevel?: IsolationLevel): Promise<void>;
+    commitTransaction(): Promise<void>;
+    rollbackTransaction(): Promise<void>;
+    private getConnection(connectionString);
+    private assertConnected(pool, timeout?);
+    execute<U>(query: Query<U>): Promise<IRecordSet<U>>;
+    execute<U>(work: (connection: MsSql.Connection) => IRecordSet<U> | PromiseLike<IRecordSet<U>>): Promise<IRecordSet<U>>;
+    private getIsolationLevel(isolationLevel);
+    private delay(ms);
+}
